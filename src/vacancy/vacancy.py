f5a346065e670cb89f23a15c04e843b1f95b27da
marseilleumirzak <galinaumerzakhova@yandex.ru>
2025-12-02 15:39:10 +0300

    vacancy

---
+++ src/vacancy/vacancy.py
@@ -0,0 +1,38 @@
+from typing import Dict, List, Optional
+
+
+class Vacancy:
+    def __init__(
+            self,
+            title: str,
+            url: str,
+            salary_from: Optional[int],
+            salary_to: Optional[int],
+            employer: str,
+            description: str = ""
+    ):
+        self.title = title,
+        self.url = url,
+        self.salary_from = salary_from,
+        self.salary_to = salary_to,
+        self.employer = employer,
+        self.description = description
+
+    @staticmethod
+    def hh_data(data: Dict) -> "Vacancy":
+        salary = data.get("salary") or {}
+        return Vacancy(
+            title=data.get("name", "Без названия"),
+            url=data.get("alternate_url", ""),
+            salary_from=data.get("from"),
+            salary_to=data.get("to"),
+            employer=data.get("employer", {}).get("name", "Неизвестно"),
+            description=(data.get("snippet") or {}).get("responsibility", "") or ""
+        )
+
+    def __str__(self):
+        salary = f"от {self.salary_from}" if self.salary_from else ""
+        if self.salary_to:
+            salary += f" до {self.salary_to}"
+        return f"{self.title} ({self.employer}\nЗарплата: {salary or 'Не указана'}\n{self.url}"
+
